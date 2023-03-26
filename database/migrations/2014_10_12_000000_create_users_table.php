a1b1945e9dbd303589c40057d8e39662c24b1dff
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-03-25 20:30:32 -0600

    API-1 Add user register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->string('userName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('lastLoginDate')->nullable();
+            $table->timestamp('createdDate')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->integer('idAddress')->nullable();
         });
     }
 
