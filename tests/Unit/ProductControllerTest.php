cce3008b0066163573028d4634db3bb15ba9bf54
Endorsmoch <A16003872@alumnos.uady.mx>
2023-05-22 20:02:46 -0600

    API-18 ProductControllerTest fixed

--- tests/Unit/ProductControllerTest.php
+++ tests/Unit/ProductControllerTest.php
@@ -5,44 +5,108 @@ namespace Tests\Unit;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class ProductControllerTest extends TestCase
 {
-    public function testIndexIsCalledOnce()
+    use RefreshDatabase;
+
+    protected function setUp(): void
     {
-        $productControllerMock = $this->getMockBuilder(ProductController::class)->onlyMethods(['index'])->getMock();
-        $productControllerMock->expects($this->once())->method('index');
-        $productControllerMock->getMethodIndex();   
+        parent::setUp();
+
+        $this->withoutMiddleware(\App\Http\Middleware\Authenticate::class);
+    }
+    
+    public function testIndexMethod()
+    {
+        Product::factory()->create([
+            'name' => 'Xbox One',
+            'description' => 'Videogame Console',
+            'manufacturer' => 'Microsoft',
+            'price' => 11000,
+            'stock' => 20 
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Nintendo Switch',
+            'description' => 'Videogame Console',
+            'manufacturer' => 'Nintendo',
+            'price' => 6500,
+            'stock' => 8
+        ]);
+
+        $this->get('/api/store/products')
+        ->assertSee('Xbox One')
+        ->assertSee('Nintendo Switch');
+       
     }
 
-    public function testStoreIsCalledOnce()
+    public function testStoreMethod()
     {
-        $request = new Request();
-        $productControllerMock = $this->getMockBuilder(ProductController::class)->onlyMethods(['store'])->getMock();
-        $productControllerMock->expects($this->once())->method('store');
-        $productControllerMock->getMethodStore($request);   
+        $data = [
+            'name' => 'PlayStation 5',
+            'description' => 'Videogame Console Next Gen',
+            'manufacturer' => 'Sony',
+            'price' => 12000,
+            'stock' => 10
+        ];
+
+        $response = $this->json('POST', 'api/store/products', $data);
+        $response->assertSee('PlayStation 5');
+    }
+    
+    public function testShowMethod()
+    {
+        Product::factory()->create([
+            'name' => 'Xbox One',
+            'description' => 'Videogame Console',
+            'manufacturer' => 'Microsoft',
+            'price' => 11000,
+            'stock' => 20 
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Nintendo Switch',
+            'description' => 'Videogame Console',
+            'manufacturer' => 'Nintendo',
+            'price' => 6500,
+            'stock' => 8
+        ]);
+
+        $this->get('/api/store/products/5')
+        ->assertSee('Nintendo Switch');
+    }
+    
+    public function testUpdateMethod()
+    {
+        Product::factory()->create([
+            'name' => 'Samsung S9',
+            'description' => 'Celular',
+            'manufacturer' => 'Samsung',
+            'price' => 17000,
+            'stock' => 28 
+        ]);
+
+        $data = ['price' => 17500];
+        $response = $this->json('PUT', 'api/store/products/6', $data);
+        $response->assertSee('Samsung S9')->assertSee(17500);
+
     }
 
-    public function testShowIsCalledOnce()
+    public function testDestroyMethod()
     {
-        $productControllerMock = $this->getMockBuilder(ProductController::class)->onlyMethods(['show'])->getMock();
-        $productControllerMock->expects($this->once())->method('show');
-        $productControllerMock->getMethodShow("1");   
-    }
-
-    public function testUpdateIsCalledOnce()
-    {
-        $request = new Request();
-        $productControllerMock = $this->getMockBuilder(ProductController::class)->onlyMethods(['update'])->getMock();
-        $productControllerMock->expects($this->once())->method('update');
-        $productControllerMock->getMethodUpdate($request,"1");  
-    }
-
-    public function testDestroyIsCalledOnce()
-    {
-        $productControllerMock = $this->getMockBuilder(ProductController::class)->onlyMethods(['destroy'])->getMock();
-        $productControllerMock->expects($this->once())->method('destroy');
-        $productControllerMock->getMethodDestroy("1"); 
+        Product::factory()->create([
+            'name' => 'Alexa',
+            'description' => 'IA for house',
+            'manufacturer' => 'Google',
+            'price' => 12000,
+            'stock' => 100 
+        ]);
+        $this->delete('/api/store/products/7')
+        ->assertSee('Product deleted successfully');
     }
 
     
